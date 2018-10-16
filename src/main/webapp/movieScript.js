3b0d401a14c04fbb0a408448bea6828f7242259a
Yegor Scherbatkin <goodronechosen@gmail.com>
2018-10-17 01:01:35 +0300

    Add seats grid

--- src/main/webapp/movieScript.js
+++ src/main/webapp/movieScript.js
@@ -1,17 +1,32 @@
+function loadSeatRow(row) {
+  $.get("movieSeatRow.html", function(value) {
+    var template = $.templates(value);
+    var html = template.render(row);
+    $("#seatBoxArea").append(html);
+  });
+}
+
 function loadSeat(seat) {
   $.get("movieSeat.html", function(value) {
     var template = $.templates(value);
     var html = template.render(seat);
-    $("#seatBoxArea").append(html);
+    $(".row" + seat["row"]).append(html);
   });
 }
 
 $("#loadSeatsBtn").on("click", function() {
   $("#welcomeTitle, #loadSeatsBtn").hide("fast");
   $("#seatsArea").show("fast");
-  for (var i = 0; i < 10; i++) {
-    var seat = {};
-    seat["place"] = "A" + i;
-    loadSeat(seat);
+  var seatLabels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+  for (var j = 0; j < 10; j++) {
+    var row = {};
+    row["row"] = seatLabels[j];
+    loadSeatRow(row);
+    for (var i = 0; i < 10; i++) {
+      var seat = {};
+      seat["pos"] = i;
+      seat["row"] = seatLabels[j];
+      loadSeat(seat);
+    }
   }
 });
