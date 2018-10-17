7cf1e293441ef841d4d1bcac887f322a596bf9a2
Yegor Scherbatkin <goodronechosen@gmail.com>
2018-10-17 03:41:08 +0300

    Add buy tickets feature

--- src/main/webapp/movieScript.js
+++ src/main/webapp/movieScript.js
@@ -52,6 +52,10 @@ function showOperatingText(type) {
     $("#seatsOutputText").text("You removed this seat from your desired seats list");
     return;
   }
+  if (type === "reserved") {
+    $("#seatsOutputText").text("You successfully brought your tikets, enjoy the movie!");
+    return;
+  }
   $("#seatsOutputText").text("There are " + freeCount + " free seats, "
       + reservedCount + " reserved and " + pickedCount + " picked now");
 }
@@ -116,3 +120,76 @@ $(document).on("click", '.pickingSeat', function () {
   }
   ticketTextUpdate();
 });
+
+//here begins pop-up function copypasted code
+$(document).ready(function() {
+  $('body').append('<div id="blackout"></div>');
+  var boxWidth = 400;
+
+  function centerBox() {
+    var winWidth = $(window).width();
+    var winHeight = $(document).height();
+    var scrollPos = $(window).scrollTop();
+    var disWidth = (winWidth - boxWidth) / 2;
+    var disHeight = scrollPos + 150;
+    $('.popup-box').css({'width' : boxWidth+'px', 'left' : disWidth+'px', 'top' : disHeight+'px'});
+    $('#blackout').css({'width' : winWidth+'px', 'height' : winHeight+'px'});
+    return false;
+  }
+
+  $(window).resize(centerBox);
+  $(window).scroll(centerBox);
+  centerBox();
+
+  $(document).on("click", '#buyTicketBtn', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    //buy ticket logic is here
+    showOperatingText("reserved");
+    var pickingCount = countSeats("picking");
+    var seats = "";
+    $(".ticketSeatsCount").text(pickingCount);
+    $(".ticketPrice").text((pickingCount * 100) + " rur");
+    $('.pickingSeat').each(function() {
+      var seatId = this.id;
+      seats = seats + seatId.substr(4, seatId.length) + ", ";
+    });
+    seats = seats.substr(0, (seats.length - 2));
+    $(".ticketSeats").text(seats);
+    $(".pickingSeat").addClass("reservedSeat");
+    $(".pickingSeat").removeClass("pickingSeat");
+    ticketTextUpdate();
+    $("#buyTicketArea").hide("fast");
+
+    //other part of pop-up code
+    var name = $(this).attr('class');
+    var id = name[name.length - 1];
+    var scrollPos = $(window).scrollTop();
+    $('#popup-box-'+id).show();
+    $('#blackout').show();
+    $('html,body').css('overflow', 'hidden');
+    $('html').scrollTop(scrollPos);
+  });
+
+  $('[class*=popup-box]').click(function(e) {
+    e.stopPropagation();
+  });
+
+  $('html').click(function() {
+    var scrollPos = $(window).scrollTop();
+    $('[id^=popup-box-]').hide();
+    $('#blackout').hide();
+    $("html,body").css("overflow","auto");
+    $('html').scrollTop(scrollPos);
+  });
+
+  $('.close').click(function() {
+    var scrollPos = $(window).scrollTop();
+    $('[id^=popup-box-]').hide();
+    $('#blackout').hide();
+    $("html,body").css("overflow","auto");
+    $('html').scrollTop(scrollPos);
+  });
+});
+//pop-up copypaste ends here
